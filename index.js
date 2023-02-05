822c5258333b03a07164a3d780b19bdd99acfa2d
sachinkumar1469 <sachinyadav1469@gmail.com>
2023-02-05 22:04:09 +0530

    Comments are added

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const express = require('express');
 const mongoose = require("mongoose");
 const expressSession = require("express-session");
 
+// To save express session in mongodb 
 const MongoDBStore = require("connect-mongodb-session")(expressSession);
 
+// Passport is used for authentication
 const passport = require("./config/passport");
 
+// Store configuration
 const store = new MongoDBStore({
     uri:'mongodb+srv://sachinyadav1469:Sachin%40123@cluster0.my3twen.mongodb.net/auth?retryWrites=true&w=majority',
     collection:'authStore'
 })
 
+// Initalizing App
 const app = express();
 
+// Setting Up connection to mongodb atlas server
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb+srv://sachinyadav1469:Sachin%40123@cluster0.my3twen.mongodb.net/auth?retryWrites=true&w=majority')
         .then(result=>{
@@ -23,16 +28,18 @@ mongoose.connect('mongodb+srv://sachinyadav1469:Sachin%40123@cluster0.my3twen.mo
         })
 
 
-
+// Template engine
 app.set('view engine','ejs');
 app.set('views','views');
 
+// Statid file routes
 app.use(express.static('public'));
 
+// To encode req body
 app.use(express.urlencoded({extended:false}));
 
 
-
+// Initializing the express session
 app.use(expressSession({
     secret:"MySecretKey",
     saveUninitialized:false,
@@ -40,15 +47,19 @@ app.use(expressSession({
     store:store
 }));
 
+// Initializing the passport js
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Auth route
 app.use("/auth",require('./routes/auth'));
 
+// Auth failure handler
 app.use("/fail",(req,res,next)=>{
-    res.send("Fail");
+    res.send("Authentication Failed");
 })
 
+// Home page route
 app.use("/",(req,res,next)=>{
     res.render(require("path").join(__dirname,"views","home"));
 })
